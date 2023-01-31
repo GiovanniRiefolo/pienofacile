f17f6deb2d8344e6d0e126f1b38e3fa3b26aaf0a
Giovanni Riefolo <giovanni@riefolo.dev>
2023-01-31 13:30:10 +0000

    adding map component

---
+++ src/components/map-search.vue
@@ -0,0 +1,18 @@
+<script setup>
+import "leaflet"
+
+const props = defineProps(['points', 'radius'])
+
+const map = L.map('map').fitWorld();
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '© OpenStreetMap'
+}).addTo(map);
+
+</script>
+
+<template>
+  <div id="map">
+  </div>
+</template>
